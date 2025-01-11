bbaf0fea0544908e67ed9930898170c5b152cd25
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-01-11 12:28:29 +0900

    Feat: RouterProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-function App() {
-  return (
-    <></>
-  );
-}
+import { RouterProvider } from 'react-router';
+
+import router from './routes/Router';
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
