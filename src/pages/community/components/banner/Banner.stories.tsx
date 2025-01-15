5b767d3513b951756605197d75c9c57f7ac48c39
goeun kim <rlarhdms6828@naver.com>
2025-01-15 10:51:31 +0900

    Chore/#38: banner 스토리북 title 변경

--- src/pages/community/components/banner/Banner.stories.tsx
+++ src/pages/community/components/banner/Banner.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Banner from './Banner';
 
 const meta: Meta<typeof Banner> = {
-  title: 'Components/Banner',
+  title: 'Components/Community Banner',
   component: Banner,
   parameters: {
     layout: 'fullscreen',
