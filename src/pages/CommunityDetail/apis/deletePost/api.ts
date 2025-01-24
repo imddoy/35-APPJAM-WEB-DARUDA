cf5f3c2067a1d60bda80f5133b6dbf294a9a09a2
imddoy <imddoy@duksung.ac.kr>
2025-01-24 18:21:16 +0900

    [!HOTFIX] deletePost 업로드

--- src/pages/CommunityDetail/apis/deletePost/api.ts
+++ src/pages/CommunityDetail/apis/deletePost/api.ts
@@ -1,9 +1,8 @@
 import { del } from '@apis/index';
-// 주석
+
 // 댓글 삭제 API
 export const delComment = async (commentId: number) => {
   const res = await del(`comments?comment-id=${commentId} `);
   return res;
 };
-
 export default delComment;
--- src/pages/CommunityDetail/apis/deletePost/queries.ts
+++ src/pages/CommunityDetail/apis/deletePost/queries.ts
@@ -3,23 +3,19 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Post } from 'src/types/post';
 
 import delComment from './api';
-// 주석
 
 export interface InfiniteQueryResponse {
   pages: CommentResponse[]; // 각 페이지의 댓글 데이터
   pageParams: number[]; // 페이지 매개변수
 }
-
 const useCommentDelete = (commentId: number, postId: string | undefined) => {
   const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: () => delComment(commentId),
     onMutate: async () => {
       await queryClient.cancelQueries({ queryKey: ['comment', postId] });
       const prevComments = queryClient.getQueryData<InfiniteQueryResponse>(['comment', postId]); // 댓글 리스트 가져오기
       const prevDetail = queryClient.getQueryData<Post>(['detailPost', postId]); // 게시글 상세 가져오기
-
       if (prevComments) {
         queryClient.setQueryData<InfiniteQueryResponse>(['comment', postId], {
           ...prevComments,
@@ -29,14 +25,11 @@ const useCommentDelete = (commentId: number, postId: string | undefined) => {
           })),
         });
       }
-
       const commentCount = prevDetail?.commentCount ? prevDetail.commentCount - 1 : 0;
-
       queryClient.setQueryData(['detailPost', postId], {
         ...prevDetail,
         commentCount: commentCount,
       });
-
       return { prevComments, prevDetail };
     },
     onError: (error, _, context) => {
@@ -50,5 +43,4 @@ const useCommentDelete = (commentId: number, postId: string | undefined) => {
     },
   });
 };
-
 export default useCommentDelete;
