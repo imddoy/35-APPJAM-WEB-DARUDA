20de907d9dd434e2f336179ee84e4d59b6923bd0
imddoy <imddoy@duksung.ac.kr>
2025-01-24 18:29:13 +0900

    deletePost 업로드

---
+++ src/pages/CommunityDetail/apis/deletePost/api.ts
@@ -0,0 +1,7 @@
+import { del } from '@apis/index';
+// 댓글 삭제 API
+export const delComment = async (commentId: number) => {
+  const res = await del(`comments?comment-id=${commentId} `);
+  return res;
+};
+export default delComment;
