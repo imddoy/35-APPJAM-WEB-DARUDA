eec079c081d186e5893cbdb42c22205ca386faf7
goeun kim <rlarhdms6828@naver.com>
2025-04-12 21:14:50 +0900

    Feat: husky 커밋 메시지 설정 [#212]

---
+++ commitlint.config.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)(?:\/#(\d+))?: (.*)$/, 
+      headerCorrespondence: ['type', 'issue', 'subject'],     
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'Init',
+        'Feat',
+        'Fix',
+        'Docs',
+        'Style',
+        'Refactor',
+        'Test',
+        'Chore',
+        'Build',
+        'Design',
+        'Comment',
+        'Rename',
+        'Remove',
+        '!HOTFIX',
+      ],
+    ],
+    'subject-empty': [
+      2,
+      'never',
+    ],
+    'type-empty': [
+      2,
+      'never'
+    ],
+  },
+};
